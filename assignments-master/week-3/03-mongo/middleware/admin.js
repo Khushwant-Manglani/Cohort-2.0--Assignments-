b6659f171780266aa06fd9ce47a271fdb0260362
Khushwant <khushwantmanglani6@gmail.com>
2024-01-23 22:26:12 +0530

    03-mongo assignment

--- assignments-master/week-3/03-mongo/middleware/admin.js
+++ assignments-master/week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,28 @@
+const { Admin } = require('../db');
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const username = req.headers.username; // abcd@gmail.com
+    const password = req.headers.password; // 123af
+
+    Admin.findOne({ 
+        username: username, 
+        password: password
+    })
+    .then((data) => {
+        if(data) {
+            // username with this password is exist in database
+            next(); 
+        } else {
+            // username with this password is doesn't exist in database
+            res.status(403).json({
+                msg: "Admin does'nt exit with this username and password"
+            });
+        }
+    });
 }
 
 module.exports = adminMiddleware;
--- assignments-master/week-3/03-mongo/middleware/user.js
+++ assignments-master/week-3/03-mongo/middleware/user.js
@@ -1,6 +1,27 @@
+const { User } = require('../db');
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const username = req.headers.username; // abcd@gmail.com
+    const password = req.headers.password; // 123af
+
+    User.findOne({ 
+        username: username, 
+        password: password
+    })
+    .then((data) => {
+        if(data) {
+            // username with this password is exist in database
+            next(); 
+        } else {
+            // username with this password is doesn't exist in database
+            res.status(403).json({
+                msg: "User does'nt exit with this username and password"
+            });
+        }
+    });
 }
 
 module.exports = userMiddleware;
